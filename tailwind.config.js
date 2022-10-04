aa22158617078834d2988744ef88fa222f90a669
rousong <recall131477@gmail.com>
2022-10-04 17:38:38 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      spacing: {
+        25: '100px',
+      },
+    },
+  },
+  plugins: [],
+}
